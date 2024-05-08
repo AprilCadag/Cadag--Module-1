a868805ed5a6521b97423ba881ed4a24038ccb8f
AprilCadag <128223760+AprilCadag@users.noreply.github.com>
2024-05-08 20:58:49 +0800

    module1.sol

--- module1.sol
+++ module1.sol
@@ -4,10 +4,22 @@ pragma solidity ^0.8.0;
 contract SafetyContract {
     address public owner;
 
-    constructo
-    
-    
+    constructor() {
+        owner = msg.sender;
+    }
+
     function inputValidation(uint256 x) public pure returns (uint256) {
         require(x != 0, "Input must not be zero");
         return x;
     }
+
+    function validateOrder(uint256 a, uint256 b) public pure returns (uint256) {
+        assert(a < b);
+        return a;
+    }
+
+    function restrictedOperation() public view {
+        require(msg.sender == owner, "Only owner can call this function");
+        revert("This function reverted");
+    }
+}
